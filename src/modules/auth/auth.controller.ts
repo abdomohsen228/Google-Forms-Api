43e1c3cce9caa0e3cd831a9a83f7f4bf74255772
abdelrhman mohsen <abdo.mohsen3457643@gmail.com>
2025-11-05 23:51:19 +0200

    add Throttle module for rate limiting

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -2,15 +2,19 @@ import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginRequestDto } from './dtos/loginRequest.dto';
 import { RegisterRequestDto } from './dtos/registerRequest.dto';
+import { SkipThrottle } from '@nestjs/throttler';
 // import { RegisterRequestDto } from './dtos/registerRequest.dto';
 
+@SkipThrottle()
 @Controller('/auth')
 export class AuthController {
   constructor(private readonly authServices: AuthService) {}
+
   @Post('/register')
   public register(@Body() body: RegisterRequestDto) {
     return this.authServices.register(body);
   }
+
   @Post('/login')
   @HttpCode(HttpStatus.OK)
   public login(@Body() body: LoginRequestDto) {
